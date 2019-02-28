29dd5664a534e7a83babd22652cb47a4f68febdd
Renee Marsland <reneemarsland@hotmail.com>
2019-02-28 13:44:16 +1030

    Update app.py
    Now have +, -, and reset buttons working properly.
    Crash bug fixed.

--- app.py
+++ app.py
@@ -15,25 +15,71 @@ class AppWindow(QMainWindow):
 
     # Functions concerning the home page.
     def home(self):
+        # Setting base life points.
+        self.basePoints = "20"
+
+        # Tracking the subtracting and adding of life points.
         self.ui.TakeLife02_pushButton.clicked.connect(partial(self.takeLife, "player02"))
         self.ui.TakeLife01_pushButton.clicked.connect(partial(self.takeLife, "player01"))
+
+        self.ui.AddLife02_pushButton.clicked.connect(partial(self.addLife, "player02"))
+        self.ui.AddLife01_pushButton.clicked.connect(partial(self.addLife, "player01"))
+
+        # Tracking reset of life points.
+        self.ui.reset_pushButton.clicked.connect(partial(self.reset))
+
         self.show()
 
     # Functions that handles adding and subtracting life from players.
     def takeLife(self, player):
+        # TODO:
+        #  Come up with a better way of deducting points so it is easier to add more than just two players to games.
+
+        # Reducing Player 02 life points.
         if player == "player02":
             print("Taking life from player 2!")
             # Getting the current life value of player 2.
             player02Counter = int(self.ui.PlayerTwoCounter_label.text())
             player02Counter = player02Counter - 1
             print("Current life = %s" % player02Counter)
-            self.ui.PlayerTwoCounter_label.setText(player02Counter) # Line errors out the window for some reason...
-            return
+            self.ui.PlayerTwoCounter_label.setText(str(player02Counter))
+
+        # Reducing Player 01 life points
         elif player == "player01":
             print("Taking life from player 1!")
+            # Getting the current life value of player 2.
+            player01Counter = int(self.ui.playerOneCounter_label.text())
+            player01Counter = player01Counter - 1
+            print("Current life = %s" % player01Counter)
+            self.ui.playerOneCounter_label.setText(str(player01Counter))
 
-    def addLife(self):
-        return "Adding life!"
+        return
+
+    def addLife(self, player):
+        # Reducing Player 02 life points.
+        if player == "player02":
+            print("Adding life to player 2!")
+            # Getting the current life value of player 2.
+            player02Counter = int(self.ui.PlayerTwoCounter_label.text())
+            player02Counter = player02Counter + 1
+            print("Current life = %s" % player02Counter)
+            self.ui.PlayerTwoCounter_label.setText(str(player02Counter))
+
+        # Reducing Player 01 life points
+        elif player == "player01":
+            print("Adding life to player 1!")
+            # Getting the current life value of player 2.
+            player01Counter = int(self.ui.playerOneCounter_label.text())
+            player01Counter = player01Counter + 1
+            print("Current life = %s" % player01Counter)
+            self.ui.playerOneCounter_label.setText(str(player01Counter))
+
+        return
+
+    def reset(self):
+        print("Resetting life points to base number")
+        self.ui.playerOneCounter_label.setText(str(self.basePoints))
+        self.ui.PlayerTwoCounter_label.setText(str(self.basePoints))
 
 
 # Main.
